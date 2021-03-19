821acca879d75556533f03e2f0ae45448c410c4d
Scott Hurst <scohurst@amazon.com>
2021-03-18 17:00:20 -0700

    Getting docker working

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3a)&9bo7m6@#^wa&*77_nelac8k%+%4y4_)7tmtj#s9n4btp&t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
